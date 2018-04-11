9c81f090f96a7ab8aab09f4b739fdca99a2c8aff
orangegrove1955 <orangegrove_1955@hotmail.com>
2018-04-11 13:45:58 +1000

    First commit, with Book class created and variables declared

---
+++ Book.swift
@@ -0,0 +1,22 @@
+//
+//  Book.swift
+//  Library of Alexandria
+//
+//  Created by Matthew Williams on 11/4/18.
+//  Copyright © 2018 mtwil3. All rights reserved.
+//
+
+import UIKit
+
+class Book: NSObject {
+    // Declaration of all variables related to Book information
+    var title: String?
+    var ISBN: Int?
+    var author: String?
+    var publisher: String?
+    var edition: Int?
+    var year: Int?
+    var genre: String?
+    var bookDesc: String?
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# library-alexandria-ios
+iOS version of the Library of Alexandria application, developed for FIT3027 at Monash University
