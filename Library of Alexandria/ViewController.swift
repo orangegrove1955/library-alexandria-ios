51bb2680badbcf59c8db12d820ae2d2252f3cdfe
orangegrove1955 <orangegrove_1955@hotmail.com>
2018-04-15 19:24:35 +1000

    Add Book interface created, text fields and toolbar buttons added to ViewController

--- Library of Alexandria/ViewController.swift
+++ Library of Alexandria/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Input text fields from Add Book page
+    @IBOutlet weak var inputTitle: UITextField!
+    @IBOutlet weak var inputISBN: UITextField!
+    @IBOutlet weak var inputAuthor: UITextField!
+    @IBOutlet weak var inputPublisher: UITextField!
+    @IBOutlet weak var inputEdition: UITextField!
+    @IBOutlet weak var inputYear: UITextField!
+    @IBOutlet weak var inputGenre: UITextField!
+    @IBOutlet weak var inputDesc: UITextField!
+    
+    // Toolbar buttons
+    @IBAction func addButton(_ sender: Any) {
+    }
+    @IBAction func cancelButton(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
