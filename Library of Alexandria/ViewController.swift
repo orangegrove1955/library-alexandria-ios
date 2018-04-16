83a219f4e76eaee1a76d97c59253ee5e45cf2a29
orangegrove1955 <orangegrove_1955@hotmail.com>
2018-04-16 17:29:37 +1000

    Added logic to ViewController to create a Book from inputs

--- Library of Alexandria/ViewController.swift
+++ Library of Alexandria/ViewController.swift
@@ -21,9 +21,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputDesc: UITextField!
     
     // Toolbar buttons
+    // Add a book
     @IBAction func addButton(_ sender: Any) {
+        // Create message to output, set to error if not all fields filled
+        var messageString = "Not all fields filled"
+        // Check all fields are completed, create book with values if they are
+        if inputTitle.text != "" && inputISBN.text != "" && inputAuthor.text != "" && inputPublisher.text != ""
+            && inputEdition.text != "" && inputYear.text != "" && inputGenre.text != "" && inputDesc.text != ""{
+            let title = inputTitle.text
+            let ISBN = Int(inputISBN.text!)
+            let author = inputAuthor.text
+            let publisher = inputPublisher.text
+            let edition = Int(inputEdition.text!)
+            let year = Int(inputYear.text!)
+            let genre = inputGenre.text
+            let desc = inputDesc.text
+            let book = Book(title:title!, ISBN: ISBN!, author:author!, publisher:publisher!, edition:edition!, yearPub:year!, genre:genre!, bookDesc:desc!)
+            messageString = book.getSummary()
+        }
+        
+        let alertController = UIAlertController(title: "Alert", message: messageString, preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+        
     }
     @IBAction func cancelButton(_ sender: Any) {
+        
     }
     
     
